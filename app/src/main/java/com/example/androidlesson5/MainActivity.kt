6facfd34c4cf943001446e01eb0d3bd3dce240e1
alibeknurgaliyev <a.nurgaliyev@mycar.kz>
2023-09-29 18:17:21 +0600

    добавил новую функцию

--- app/src/main/java/com/example/androidlesson5/MainActivity.kt
+++ app/src/main/java/com/example/androidlesson5/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.androidlesson5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showLog()
+    }
+
+    private fun showLog(){
+        Log.d("TAG","Hello World!")
     }
 }
-
